57632dbcde1b16839904ba014a6b806815e3b979
kobezsolomon <154307416+kobezsolomon@users.noreply.github.com>
2024-06-04 13:28:49 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,29 @@
+const dropdown = document.querySelector('.dropdown');
+const minus = document.querySelector('.mius');
+const plus = document.querySelector('.plus');
+let addonemiusone = document.querySelector('.addonemiusone').innerHTML;
+console.log(addonemiusone);
+dropdown.style.display="none"
+function closem() {
+    dropdown.style.display="none"
+}
 function displaymanybar() {
+    dropdown.style.display="flex"
+}
+let stoerer = 0;
+function addone(){
+ 
+    stoerer++;
+    console.log(stoerer);
+    document.querySelector('.addonemiusone').innerHTML = stoerer
     
+  
+}
+function miusone(){
+ 
+    stoerer--;
+    console.log(stoerer);
+    document.querySelector('.addonemiusone').innerHTML = stoerer
+    
+  
 }
